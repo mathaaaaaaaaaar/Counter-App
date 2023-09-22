17fe39917f3419250fb245e11cdcb42ff3ae25c4
Shaik Mathar Syed <shaik.mathar99@gmail.com>
2023-09-22 18:20:34 -0400

    Pushed code and set up git repo

--- PROG8471 - Assignment 1/ViewController.swift
+++ PROG8471 - Assignment 1/ViewController.swift
@@ -8,12 +8,34 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    var counterVal = 0
+    var counterStep = 1
+    @IBOutlet weak var stepStatus: UILabel!
+    @IBOutlet weak var counterLabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        counterLabel.text = String(counterVal)
+        stepStatus.text = String("Current Step = 1")
     }
-
+    @IBAction func counterAdd(_ sender: Any) {
+        counterVal += counterStep
+        counterLabel.text = String(counterVal)
+    }
+    @IBAction func counterMinus(_ sender: Any) {
+        counterVal -= counterStep
+        counterLabel.text = String(counterVal)
+    }
+    @IBAction func counterReset(_ sender: Any) {
+        counterVal = 0
+        counterStep = 1
+        counterLabel.text = String(counterVal)
+        stepStatus.text = String("Current Step = 1")
+    }
+    @IBAction func counterChangeStep(_ sender: Any) {
+        counterStep = 2;
+        stepStatus.text = String("Current Step = 2")
+    }
 
 }
 
